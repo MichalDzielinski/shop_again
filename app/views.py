e64abd6af23c36d937f28a692895c222a0b99f84
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-15 07:13:41 +0200

    Another one

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
 
-# Create your views here.
+def index(request):
+    pro_obj = Product.objects.all()
+
+    item_name = request.GET.get('item_name')
+    if item_name != "" and item_name is not None:
+        pro_obj = Product.objects.filter(title__icontains=item_name)        
+
+    paginator = Paginator(pro_obj, 3)
+    page = request.GET.get('page')
+    pro_obj = paginator.get_page(page)
+
+
+    context = {'pro_obj': pro_obj}
+    return render(request, 'shop/index.html', context )
